69b3b1b105d1664429936c9331f87c8f2198bffa
hyoudoukouji / ひょんどうこうじ <85467106+hyoudoukouji@users.noreply.github.com>
2025-09-09 22:51:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("enterBtn").addEventListener("click", function() {
-  alert("You shouldn't have clicked that...");
-  document.body.style.backgroundColor = "#000000";
-  document.querySelector(".intro").textContent = "The spiral has begun. There's no escape.";
+  document.body.style.backgroundColor = "#000";
+  document.querySelector(".intro").textContent = "You clicked it. The spiral is inside you now.";
   document.querySelector(".main-img").src = "https://i.imgur.com/yourScaryImage.jpg";
+  alert("The curse has awakened...");
 });
